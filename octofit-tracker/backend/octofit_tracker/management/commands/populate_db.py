28ba642db569b0c89123810124ce15d8ed6b4489
Illia Dehodiuk <166947268+IlliaDhD@users.noreply.github.com>
2026-01-06 18:29:52 +0000

    Add project and db

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,66 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create_user(username='ironman', email='ironman@marvel.com'),
+            User.objects.create_user(username='spiderman', email='spiderman@marvel.com'),
+            User.objects.create_user(username='batman', email='batman@dc.com'),
+            User.objects.create_user(username='wonderwoman', email='wonderwoman@dc.com'),
+        ]
+
+        # Create activities
+        Activity.objects.create(name='Run', user_email='ironman@marvel.com')
+        Activity.objects.create(name='Swim', user_email='spiderman@marvel.com')
+        Activity.objects.create(name='Fly', user_email='batman@dc.com')
+        Activity.objects.create(name='Lift', user_email='wonderwoman@dc.com')
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='Marvel', points=200)
+        Leaderboard.objects.create(team='DC', points=180)
+
+        # Create workouts
+        Workout.objects.create(name='Cardio', description='High intensity running')
+        Workout.objects.create(name='Strength', description='Weight lifting')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db populated with test data'))
---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,28 @@
+from djongo import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    class Meta:
+        app_label = 'octofit_tracker'
